f87869c86939a21a446d373b40a57244ca3aed88
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-04-04 20:06:03 +0300

    Добавьте файлы проекта.

---
+++ HomeWork9.1/Program.cs
@@ -0,0 +1,11 @@
+﻿//1 
+int result1 = 8 & 3;
+int result2 = 22 & 4;
+Console.WriteLine($"1) Результат логических умножений: {result1}, {result2}");
+//2
+int result3 = 65 | 29;
+int result4 = 44 | 12;
+Console.WriteLine($"2) Результат логических сложений: {result3}, {result4}");
+//3
+int result5 = 249 ^ 8;
+Console.WriteLine($"3) Результат логического шифрования: {result5}");
